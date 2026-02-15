b14c2b08418b142e738d6831ec7a25bc482fe34a
RAGASUDHA B <ragasudha@student.tce.edu>
2026-02-15 10:56:45 +0000

    learning new things

---
+++ vite-project/client/src/signup.jsx
@@ -0,0 +1,17 @@
+// Signup.jsx
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
