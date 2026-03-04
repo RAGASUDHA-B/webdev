fd7465cf752d6daf6041ae3b7e02112c221dd14a
RAGASUDHA B <ragasudha@student.tce.edu>
2026-03-04 04:33:21 +0000

    updated the main using sample react code

--- vite-project/client/src/App.jsx
+++ vite-project/client/src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Simple Counter App</h1>
-      <h2>{count}</h2>
-
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
-
-      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
-        Decrease
-      </button>
+    <div className="App">
+      <h1>Hello World!</h1>
     </div>
   );
 }
--- vite-project/client/src/main.jsx
+++ vite-project/client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App.jsx'
-
+import {createRoot} from 'react-dom/client'
+const m=(
+  <table>
+    <tr>
+      <th>class</th>
+    </tr>
+    <tr>
+      <td>c</td>
+    </tr>
+    
+  </table>
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  m
 )
