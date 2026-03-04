eaa3a5487f38e9af63a3be2d73c263cb05b0589c
RAGASUDHA B <ragasudha@student.tce.edu>
2026-03-04 03:43:20 +0000

    updated version

--- project1/backend/server.js
+++ project1/backend/server.js
@@ -1,18 +1,23 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");//import all 
-require("dotenv").config(); 
-const app=express(); //creating app
-app.use(cors());//creating link between frontend and backend
-app.use(express.json());//read json file
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err));
-app.use("/api/students",require("./routes/studentRoutes"));
+const express = require("express");
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+  res.send("Server is running successfully!");
 });
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{  //start the server
-    console.log(`server running on port ${PORT}`);
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "GET request working",
+    status: "success"
+  });
+});
+app.post("/api/user", (req, res) => {
+  const userData = req.body;
+  res.json({
+    message: "POST request received successfully",
+    data: userData
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- vite-project/client/src/App.jsx
+++ vite-project/client/src/App.jsx
@@ -1,9 +1,20 @@
-import Signup from "./signup";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <Signup />
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
     </div>
   );
 }
